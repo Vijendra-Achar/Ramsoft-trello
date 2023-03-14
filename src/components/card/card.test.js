5334ca9283662a64ebcdd61b7c5434b80db5cc5d
Vijendra-Achar <vijendrachar.m@gmail.com>
2023-03-14 18:45:04 +0530

    Test cases updated

--- src/components/card/card.test.js
+++ src/components/card/card.test.js
@@ -2,21 +2,27 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import Card from "./index";
 
+const props = {
+  heading: "heading",
+  desc: "description",
+  deadline: "2 days",
+};
+
 describe("The Card's elements", () => {
   it("Card heading renders as expected", () => {
-    render(<Card heading="heading" desc="desc" deadline="2 days" />);
+    render(<Card {...props} />);
     const headingElement = screen.getByText("heading");
     expect(headingElement).toBeTruthy();
   });
 
   it("Card description renders as expected", () => {
-    render(<Card heading="heading" desc="desc" deadline="2 days" />);
-    const descElement = screen.getByText("desc");
+    render(<Card {...props} />);
+    const descElement = screen.getByText("description");
     expect(descElement).toBeInTheDocument();
   });
 
   it("Card deadline renders as expected", () => {
-    render(<Card heading="heading" desc="desc" deadline="2 days" />);
+    render(<Card {...props} />);
     const deadlineElement = screen.getByText("2 days");
     expect(deadlineElement).toBeTruthy();
   });
